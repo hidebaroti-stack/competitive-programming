e3810aea35cbf6a7c9ad4231e0c837f67e9ff76c
Debaroti Deb Nath <hidebaroti@gmail.com>
2026-05-23 22:57:45 +0600

    Java Stdin and Stdout I, Done.

---
+++ HackerRank/Java-Track/Stdin_Stdout_I.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        //Creating a scanner object
+        Scanner scanner = new Scanner(System.in);
+        //Input
+        int myInt1 = scanner.nextInt();
+        int myInt2 = scanner.nextInt();
+        int myInt3 = scanner.nextInt();
+        //Closing Scanner object
+        scanner.close();
+        //Output
+        System.out.println(myInt1);
+        System.out.println(myInt2);
+        System.out.println(myInt3);
+
+    }
+}
