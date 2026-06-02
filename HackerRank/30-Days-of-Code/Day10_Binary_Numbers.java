573bb7966457a9b47e64bb4a6008e6f3969979b0
Debaroti Deb Nath <hidebaroti@gmail.com>
2026-06-02 12:50:58 +0600

    Day10_Binary_Numbers.java , Done.

---
+++ HackerRank/30-Days-of-Code/Day10_Binary_Numbers.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int remainder;
+        int currentCount = 0;
+        int maxCount = 0;
+
+        while (n > 0) {
+            remainder = n % 2;
+
+            if (remainder == 1) {
+                currentCount++;
+                if (currentCount > maxCount) {
+                    maxCount = currentCount;
+                }
+            } else {
+                currentCount = 0;
+            }
+
+            n /= 2;
+        }
+
+        System.out.println(maxCount);
+
+
+        bufferedReader.close();
+    }
+}
