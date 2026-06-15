92a36edc1b9dc1a2c0621dc547d008d03eccfc94
Debaroti Deb Nath <hidebaroti@gmail.com>
2026-06-15 22:32:10 +0600

    Reverse_a_String.java, Done.

---
+++ GeeksForGeeks/Strings/Reverse_a_String.java
@@ -0,0 +1,18 @@
+// User function Template for Java
+
+class Solution {
+    public static String reverseString(String s) {
+        // code here
+        char[] chars = s.toCharArray();
+        int left = 0;
+        int right = chars.length - 1;
+        while (left < right) {
+            char temp = chars[left];
+            chars[left] = chars[right];
+            chars[right] = temp;
+            left++;
+            right--;
+        }
+        return new String(chars);
+    }
+}
