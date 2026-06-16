b2446c286bac3d7e8d9213ecf316ea697bf8d84e
Debaroti Deb Nath <hidebaroti@gmail.com>
2026-06-16 21:46:56 +0600

    Reverse_Digits.java, Done.

---
+++ GeeksForGeeks/Math/Reverse_Digits.java
@@ -0,0 +1,13 @@
+// User function Template for Java
+
+class Solution {
+    public int reverseDigits(int n) {
+        // Code here
+        int reverse = 0;
+        while (n != 0) {
+            reverse = reverse * 10 + (n % 10);
+            n /= 10;
+        }
+        return reverse;
+    }
+}
