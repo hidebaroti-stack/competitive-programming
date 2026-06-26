80ddd927adf268c253ff844f863dc759d8613601
Debaroti Deb Nath <hidebaroti@gmail.com>
2026-06-26 19:47:00 +0600

    Richest_Customer_Wealth_1672.java, Done.

---
+++ LeetCode/Arrays/Richest_Customer_Wealth_1672.java
@@ -0,0 +1,14 @@
+class Solution {
+    public static int maximumWealth(int[][] accounts) {
+
+        int max = 0;
+        for (int i = 0; i < accounts.length; i++) {
+            int sum = 0;
+            for (int j = 0; j < accounts[i].length; j++) {
+                sum += accounts[i][j];
+            }
+            if (max < sum) max = sum;
+        }
+        return max;
+    }
+}
