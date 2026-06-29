8fc16c7ee7622db8acc3bcfce1ebe32419f102cf
Debaroti Deb Nath <hidebaroti@gmail.com>
2026-06-29 22:25:45 +0600

    Intersection_of_Two_Arrays_II_350.java, Done.

---
+++ LeetCode/Arrays/Intersection_of_Two_Arrays_II_350.java
@@ -0,0 +1,28 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        List<Integer> list = new ArrayList<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : nums1) {
+            if (map.containsKey(num)) {
+                map.put(num, map.get(num) + 1);
+            } else {
+                map.put(num, 1);
+            }
+        }
+        for (int num : nums2) {
+            if (map.containsKey(num) && map.get(num) > 0) {
+                list.add(num);
+                map.put(num, map.get(num) - 1);
+            }
+        }
+        int[] result = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            result[i] = list.get(i);
+        }
+        return result;
+    }
+}
