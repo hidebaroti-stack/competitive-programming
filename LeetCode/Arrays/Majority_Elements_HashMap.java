dfc3d2019cb9875c2f60a5e2ee375c14ed57bc88
Debaroti Deb Nath <hidebaroti@gmail.com>
2026-07-10 21:49:22 +0600

    Majority_Elements_HashMap.java, Done.

---
+++ LeetCode/Arrays/Majority_Elements_HashMap.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i : nums) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+        for (int key : map.keySet()) {
+            if (map.get(key) > nums.length / 2) return key;
+        }
+        return -1;
+    }
+}
