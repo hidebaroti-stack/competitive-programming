e2a11a5711212fea7ae37f91db3a5775b3b46a60
Debaroti Deb Nath <hidebaroti@gmail.com>
2026-07-16 12:59:14 +0600

    Happy_Number_HashSet.java, Done.

---
+++ LeetCode/Math/Happy_Number_HashSet.java
@@ -0,0 +1,23 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    private int sumOfSquares(int n) {
+        int sum = 0;
+        while(n > 0) {
+            int digit = n % 10;
+            sum += digit * digit;
+            n /= 10;
+        }
+        return sum;
+    }
+    public boolean isHappy(int n) {
+        Set<Integer> seen = new HashSet<>();
+
+        while (n != 1 && !seen.contains(n)) {
+            seen.add(n);
+            n = sumOfSquares(n);
+        }
+        return n == 1;
+    }
+}
