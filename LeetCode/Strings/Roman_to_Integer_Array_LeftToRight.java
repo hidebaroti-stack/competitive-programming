9de99106b51e3d183b0603cde988b37b98cc4a41
Debaroti Deb Nath <hidebaroti@gmail.com>
2026-07-17 21:00:34 +0600

    Roman_to_Integer_Array_LeftToRight.java, Done.

---
+++ LeetCode/Strings/Roman_to_Integer_Array_LeftToRight.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int romanToInt(String s) {
+        int[] values = new int[128];
+        values['I'] = 1;
+        values['V'] = 5;
+        values['X'] = 10;
+        values['L'] = 50;
+        values['C'] = 100;
+        values['D'] = 500;
+        values['M'] = 1000;
+
+        int result = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int value = values[s.charAt(i)];
+            if (i + 1 < s.length() && value < values[s.charAt(i + 1)]) {
+                result -= value;
+            } else {
+                result += value;
+            }
+        }
+        return result;
+    }
+}
