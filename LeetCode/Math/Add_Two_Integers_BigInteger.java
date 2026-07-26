2d384d0b435f54c938a7d37e3f1e63b0770f6f27
Debaroti Deb Nath <hidebaroti@gmail.com>
2026-07-26 22:30:23 +0600

    Add_Two_Integers_BigInteger.java, Done.

---
+++ LeetCode/Math/Add_Two_Integers_BigInteger.java
@@ -0,0 +1,6 @@
+import java.math.BigInteger;
+class Solution {
+    public int sum(int num1, int num2) {
+        return java.math.BigInteger.valueOf(num1).add(java.math.BigInteger.valueOf(num2)).intValue();
+    }
+}
