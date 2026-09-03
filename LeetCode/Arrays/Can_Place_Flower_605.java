c0c5c6df6272735a79683fc583178310b772eeac
hidebaroti-stack <hidebaroti@gmail.com>
2026-09-03 22:00:24 +0600

    Can_Place_Flower_605.java, Done.

---
+++ LeetCode/Arrays/Can_Place_Flower_605.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int count = 0;
+        for (int i = 0; i < flowerbed.length; i++) {
+            if (flowerbed[i] == 0) {
+                int prev = (i == 0) ? 0 : flowerbed[i - 1];
+                int next = (i == flowerbed.length - 1) ? 0 : flowerbed[i + 1];
+                if (prev == 0 && next == 0) {
+                    flowerbed[i] = 1; // planting flower
+                    count++;
+                }
+            }
+        }
+        return count >= n;
+    }
+}
